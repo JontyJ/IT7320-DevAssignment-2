e76273cdc9b457c2d484dc6e3e527eaf22eec941
jonty.johnston <jonty.johnston@B204-06.soit.local>
2018-08-22 14:08:36 +1200

    Inital commit - Database connection and display working

---
+++ Assignment2/src/main/java/IT7320/Assignment2/Main.java
@@ -0,0 +1,13 @@
+package IT7320.Assignment2;
+
+import DBConnection.DBConnection;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DBConnection test = new DBConnection();
+		test.displayDB();
+
+	}
+
+}
