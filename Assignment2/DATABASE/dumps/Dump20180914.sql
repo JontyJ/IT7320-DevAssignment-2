84fddb40631040ca9d0d5600156d2e9d8dbfadb4
Jonty <jontyzj@gmail.com>
2018-09-14 22:26:28 +1200

    Added Auto_Increment to database dump

--- Assignment2/DATABASE/dumps/Dump20180914.sql
+++ Assignment2/DATABASE/dumps/Dump20180914.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `fines`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `fines` (
-  `fine_id` int(11) NOT NULL,
+  `fine_id` int(11) NOT NULL AUTO_INCREMENT,
   `parking_id` int(11) DEFAULT NULL,
   `number_plate` varchar(6) DEFAULT NULL,
   `timestamp_start` timestamp NULL DEFAULT NULL,
